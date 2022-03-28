a53414457da80c0d988a3d27d4d80e35cc96d92c
Arthur <arturmedvedev00@mail.ru>
2022-03-28 13:05:17 +0300

    refactoring argcounter

--- argcnt.sh
+++ argcnt.sh
@@ -4,9 +4,13 @@
 echo "$# this is a number of args!"
 echo "!"
 echo "!"
+cnt=("arg1" "arg2" "arg3" "arg4" "arg5" "arg6" "arg7")
+args=($1 $2 $3 $4 $5 $6 $7)
 
-for arg
-do
-    echo $arg
-    echo "!" 
+
+for (( i=0, j=0; i<7; i++, j++ ))
+do 
+    echo " ${cnt[i]} is ${args[j]} "
 done
+
+
